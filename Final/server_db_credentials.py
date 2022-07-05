2697c70f330300d1a636bd95eb103142ac9bbb4b
Makusm1990 <Markusx04@gmail.com>
2022-07-05 17:24:55 +0200

    update

--- Final/server_db_credentials.py
+++ Final/server_db_credentials.py
@@ -1,11 +1,11 @@
 import psycopg2
 
 # Path and credentials for PostgreSQL 
-HOSTNAME = "localhost"   # Enter the name of the host where the PostgreSQL databse is located.
-DATABASE = "Notfall"   # The name of your database
+HOSTNAME = "localhost"  # Enter the name of the host where the PostgreSQL databse is located.
+DATABASE = "Notfall"    # The name of your database
 USERNAME = "postgres"   # PostgreSQL Username 
-PWD = ""        # PostgreSQL Password
-PORT_ID = "5432"    # PostgreSQL PORT
+PWD = ""                # PostgreSQL Password
+PORT_ID = "5432"        # PostgreSQL PORT
 
 # connection postgreSQL
 con_postgreSQL = psycopg2.connect(
