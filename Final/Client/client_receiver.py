d7b28f86f398278148245d5797ca2fc93a658d5a
Makusm1990 <Markusx04@gmail.com>
2022-07-08 13:06:25 +0200

    update

--- Final/Client/client_receiver.py
+++ Final/Client/client_receiver.py
@@ -1,48 +1,88 @@
 import socket
 import winsound
 import json
+from xmlrpc.client import boolean
 import pystray
 import PIL.Image
 import tkinter as tk
-import threading
 import os
-
-from client_sender import sending as alarm_from_taskbar
+import signal
+import multiprocessing
+from multiprocessing import Process,freeze_support
+import threading
+from ctypes import *
 from tkinter import messagebox
-
 
 HOSTNAME = socket.gethostname()   
 IP_ADDRESS = socket.gethostbyname(HOSTNAME)   
 PORT = 8080
 CONFIG = json.load(open(r'\\dc01\netlogon\Notfall\configure.json'))
-SYSTRAY_ICON = PIL.Image.open(r"\\dc01\netlogon\Notfall\Notfall.ico")
-
-SOCKET_PARM = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-SOCKET_PARM.bind((IP_ADDRESS, PORT))
-SOCKET_PARM.listen(5)
+SYSTRAY_ICON = PIL.Image.open(r"D:\x_Notfall\Final\Client\alarm_icon.png")
 
 
 class SystemtrayIcon:
-   def __init__(self):
-      self.thread_systray = threading.Thread(target=self.systray)
-      self.thread_systray.start()
+   def __init__(self,*args):
+      self.systray(self,*args)
 
    def on_clicked_alarm(self):
-      alarm_from_taskbar()    
+      sending()   
    
    def version_info(self):
       print("Version 1.0.0")
+   
+   def exit_session(self,*args):
+      parent_id = os.getpid()
+      print(parent_id)
 
-   def systray(self):
+   def systray(self,*args):
       self.icon = pystray.Icon("alarm", SYSTRAY_ICON, menu=pystray.Menu(
                pystray.MenuItem("ALARM!!!",SystemtrayIcon.on_clicked_alarm),
                pystray.MenuItem("Version",SystemtrayIcon.version_info),
-               pystray.MenuItem("Exit", exit_session),
+               pystray.MenuItem("Exit", SystemtrayIcon.exit_session),
             ))
       self.icon.run()
+   
+   
+class Socketlisten:
+   def __init__(self,*args):
+      self.listen()
 
-def exit_session():
-   os._exit(1)
+   def listen(self):
+      self.SOCKET_PARM = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.SOCKET_PARM.bind((IP_ADDRESS, PORT))
+      while True: 
+         self.SOCKET_PARM.listen(1) 
+         clientsocket, address = self.SOCKET_PARM.accept()
+         for x in CONFIG:
+            device = CONFIG[x]["Name"]
+            if address[0] == CONFIG[x]["IPAddress"]:         
+               print(f"Alarm from {device}")
+               alarm(device)
+
+class USB_Taster:
+   def __init__(self,*args):
+      self.usb_listen()
+      
+   def usb_listen(self):
+    mydll=windll.LoadLibrary(r"\\dc01\netlogon\Notfall\USBaccessX64.dll")
+    cw=mydll.FCWInitObject()
+    devCnt=mydll.FCWOpenCleware(0)
+
+    if (devCnt != 1) :
+        print("no device found")
+        exit()
+
+    state = 0
+
+    while (1) :
+        contact = mydll.FCWGetContact(0, 0)
+        newstate = contact & 1
+
+        if (state != newstate):
+            if state == 1:
+                sending()
+
+        state = newstate
 
 def alarm(device):
    root= tk.Tk()
@@ -78,20 +118,41 @@ def alarmsound():
       freq = 880  # Hz
       winsound.Beep(freq, duration)
 
-   
-def listen(): 
-   clientsocket, address = SOCKET_PARM.accept()
-   for x in CONFIG:
-      device = CONFIG[x]["Name"]
-      if address[0] == CONFIG[x]["IPAddress"]:         
-         print(f"Alarm from {device}")
-         alarm(device)
-  
+def send_threads(device):
+    print(device)
+    try:
+        CONFIG[device]["Name"] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        CONFIG[device]["Name"].connect((CONFIG[device]["IPAddress"],PORT))
+    except ConnectionRefusedError as error:
+        print(CONFIG[device]["IPAddress"],error)
+
+def sending():     
+    for device in CONFIG:
+        try:
+            send = threading.Thread(target=send_threads, args=(device,))
+            send.start()
+        except Exception:
+            continue
+
+def create_processes():
+   a = Process(target=SystemtrayIcon)
+   b = Process(target=Socketlisten)
+   c = Process(target=USB_Taster)   
+   a.start()
+   b.start()
+   c.start()
 
 if __name__ == "__main__": 
-   SystemtrayIcon()
-   while True:
-      listen()
+   create_processes() 
+   parent = multiprocessing.current_process()
+   print(f"Parent_ID: {parent.pid}")
+
+
+      
+   
+      
+      
+
    
       
       
