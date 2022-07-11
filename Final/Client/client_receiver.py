0ea93ed6aa45f1c9ac6edd58ad4fcb38ca81c839
Makusm1990 <Markusx04@gmail.com>
2022-07-11 17:21:33 +0200

    update

--- Final/Client/client_receiver.py
+++ Final/Client/client_receiver.py
@@ -12,14 +12,14 @@ import multiprocessing
 
 from ctypes import *
 from tkinter import messagebox
-from multiprocessing import Process,Queue
+from multiprocessing import Process,Queue,freeze_support
 
 
 HOSTNAME = socket.gethostname()   
 IP_ADDRESS = socket.gethostbyname(HOSTNAME)   
 PORT = 8080
 CONFIG = json.load(open(r'\\dc01\netlogon\Notfall\configure.json'))
-SYSTRAY_ICON = PIL.Image.open(r'\\dc01\netlogon\Notfall\logo.png')
+SYSTRAY_ICON = PIL.Image.open(r'D:\x_Notfall\Final\Client\logo.png')
 
 
 class SystemtrayIcon:
@@ -77,7 +77,7 @@ class USB_Taster:
       self.usb_listen()
       
    def usb_listen(self):
-    mydll=windll.LoadLibrary(r"\\dc01\netlogon\Notfall\USBaccessX64.dll")
+    mydll=windll.LoadLibrary(r"D:\x_Notfall\Final\Client\USBaccessX64.dll")
     cw=mydll.FCWInitObject()
     devCnt=mydll.FCWOpenCleware(0)
 
@@ -157,6 +157,7 @@ def create_processes(parent_pid):
    c.start()
 
 if __name__ == "__main__": 
+   freeze_support()
    queue = Queue()
    parent = multiprocessing.current_process()
    parent_pid = parent.pid
