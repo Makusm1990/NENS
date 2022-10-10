fdda5a336da15979e8932d93964174f6831e97ab
Makusm1990 <Markusx04@gmail.com>
2022-10-10 15:26:42 +0200

    Testalarm

--- Final/Client/client_modul.py
+++ Final/Client/client_modul.py
@@ -1,4 +1,3 @@
-from cgitb import text
 import os
 import json
 import socket
@@ -162,23 +161,28 @@ def minimize_all_windows(hwnd, ctx):                                          # 
         if win32gui.GetWindowText(hwnd) != "":
             win32gui.ShowWindow(hwnd, win32con.SW_MINIMIZE)
 
-
 def visual_alert(device):                                                     # Visual alert -> Window on screen 99%. Cant be closed while function alarmsoud is running (6,4sec)
    win32gui.EnumWindows(minimize_all_windows, None)                           # Minimize all open windows.
    date_time = datetime.now().strftime("%H:%M:%S")                            # current date and time
 
    root= tk.Tk()
-   root.title('NOTFALL')
+   if device == "NENS Server":
+      root.title('TEST ALARM')
+      root.configure(background='green')
+      label_INFO = tk.Label(master=root, text=f"TEST ALARM \n{device}",font=('Arial 70 bold'),bg="yellow",fg="black")
+      label_INFO.place(height=250, width=width, y=(height*0.08))
+   else:
+      root.title('NOTFALL')
+      root.configure(background='red')
+      label_INFO = tk.Label(master=root, text=f"NOTFALL \n{device}",font=('Arial 70 bold'),bg="yellow",fg="black")
+      label_INFO.place(height=250, width=width, y=(height*0.08))
+
    root.iconbitmap(r'\\dc01\netlogon\Notfall\Logos\logo_small.ico')
-   root.configure(background='red')
 
    width = round((root.winfo_screenwidth())*0.99)
    height = round((root.winfo_screenheight())*0.99)
 
    root.geometry(f"{width}x{height}+0+0")
-
-   label_INFO = tk.Label(master=root, text=f"NOTFALL \n{device}",font=('Arial 70 bold'),bg="yellow",fg="black")
-   label_INFO.place(height=250, width=width, y=(height*0.08))
 
    date = tk.Label(root,text=f"Ausgelöst um: {date_time}",font=('Arial 70 bold'),bg="yellow",fg="black")
    date.place(height=125,width=width, y=(height*0.8))
